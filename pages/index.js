1bf4c5655fd902d7c5443787679702918b291a82
Sergio Gabriel <gabrielambe13@gmail.com>
2020-11-09 12:34:12 -0300

    nova chamda na home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function home() {
-    return <div><img src="https://i.imgur.com/9UClQpw.png"></img></div>
+    return <div><h2>Página inicial</h2>
+        <img src="https://i.imgur.com/9UClQpw.png"></img></div>
 }
 
 export default home
